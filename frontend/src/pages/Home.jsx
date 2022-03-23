024352804c253f0a82c1b45778f3dd34f9eedd5b
Steve <shlc82@gmail.com>
2022-03-22 22:26:23 -0400

    adds UI to register page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import Header from '../components/Header'
 
 function Home(){
     const navigate = useNavigate()
@@ -14,7 +15,13 @@ function Home(){
         }
     }, [user,navigate])
 
-    return <div>Home</div>
+    return (
+        <>
+        <Header />
+        <div>Home</div>
+        
+        </>
+    )
 }
 
 export default Home
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,10 +2,11 @@ import { useState,useEffect } from 'react'
 import {FaUser} from'react-icons/fa'
 import React from 'react'
 import {useSelector, useDispatch} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
 import {register,reset} from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
+import LeftBanner from '../components/LeftBanner'
 
 function Register(){
     const [formData, setFormData] = useState ({
@@ -65,68 +66,80 @@ function Register(){
     }
 
     return <>
-        <section className='heading'>
-            <h1> 
-                <FaUser /> Register
-            </h1>
-            <p>Please create an account</p>
-        </section>
-        <section className='form'>
-            <form onSubmit={onSubmit} >
-                <div className="form-group">
-                    <input 
-                    type="text" 
-                    className="form-control" 
-                    id='username'
-                    name='username' 
-                    value={username} 
-                    placeholder='Enter your username' 
-                    onChange={onChange}/>
-                </div>
-                <div className="form-group">
-                    <input 
-                    type="email" 
-                    className="form-control" 
-                    id='email'
-                    name='email' 
-                    value={email} 
-                    placeholder='Enter your email address' 
-                    onChange={onChange}/>
-                </div>
-                <div className="form-group">
-                    <input 
-                    type="password" 
-                    className="form-control" 
-                    id='password'
-                    name='password' 
-                    value={password} 
-                    placeholder='Enter password' 
-                    onChange={onChange}/>
-                </div>
-                <div className="form-group">
-                    <input 
-                    type="password" 
-                    className="form-control" 
-                    id='password2'
-                    name='password2' 
-                    value={password2} 
-                    placeholder='confirm password' 
-                    onChange={onChange}/>
-                </div>
-                <div className="form-group">
-                    <select name="role" id="role" value = {role} onChange={onChange}>
-                        <option value="">--Please Choose and option--</option>
-                        <option value="customer">Customer</option>
-                        <option value="service provider">Service Provider</option>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <button type='submit' className='btn btn-block'>Submit</button>
-                </div>
-            </form>
-        
-
-        </section>
+    <LeftBanner/>
+    <section className='rightbanner'>
+        <div className='registercontainer'>
+            <section className='heading'>
+                <h1> 
+                    <FaUser /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+            <section className='form'>
+                <form onSubmit={onSubmit} >
+                    <div className="form-group">
+                        <input 
+                        type="text" 
+                        className="form-control" 
+                        id='username'
+                        name='username' 
+                        value={username} 
+                        placeholder='Enter your username' 
+                        onChange={onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input 
+                        type="email" 
+                        className="form-control" 
+                        id='email'
+                        name='email' 
+                        value={email} 
+                        placeholder='Enter your email address' 
+                        onChange={onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input 
+                        type="password" 
+                        className="form-control" 
+                        id='password'
+                        name='password' 
+                        value={password} 
+                        placeholder='Enter password' 
+                        onChange={onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input 
+                        type="password" 
+                        className="form-control" 
+                        id='password2'
+                        name='password2' 
+                        value={password2} 
+                        placeholder='confirm password' 
+                        onChange={onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <select name="role" id="role" value = {role} onChange={onChange}>
+                            <option value="">--Please Choose and option--</option>
+                            <option value="customer">Customer</option>
+                            <option value="service provider">Service Provider</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <button type='submit' className='btn btn-block'>Submit</button>
+                    </div>
+                    <div className='form-group'>
+                        <ul>
+                            <li>
+                                 Already have an accoutn?<Link to='/Login'>
+                                                            <u>Login</u>  
+                                                        </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </section>
     </>
 }
 
